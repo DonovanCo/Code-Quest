629cf4485383d70c36cce539069945bd06bcee52
cole.donovan <cole.donovan@MU-C3-10.mullen.pvt.k12.co.us>
2015-04-10 09:08:27 -0600

    concat

--- src/Prob12.java
+++ src/Prob12.java
@@ -9,8 +9,11 @@ public class Prob12
 	public static void main(String[] args) throws IOException
 		{
 		Scanner file = new Scanner( new File( "Prob12.in.txt" ) );
-	      String str =file.     
-	      System.out.println(str);
+	    String str ="";
+	    while(file.hasNextLine())
+	    	{
+	    	str.concat(file.nextLine());
+	    	}
 	   }
 
 		
