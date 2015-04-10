d39b42d257beb91fc9ab550c082802ceaafc59ed
cole.donovan <cole.donovan@MU-C3-10.mullen.pvt.k12.co.us>
2015-04-10 12:28:37 -0600

    work

--- src/Prob12.java
+++ src/Prob12.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -10,9 +11,34 @@ public class Prob12
 		{
 		Scanner file = new Scanner( new File( "Prob12.in.txt" ) );
 	    String str ="";
+	    int count = 0;
+	    ArrayList<Integer> spacing = new ArrayList<Integer>();
+	    int lastSize=0;
+	   
 	    while(file.hasNextLine())
 	    	{
-	    	str.concat(file.nextLine());
+	    	str+=file.nextLine();
+    		System.out.println(str);
+	    	spacing.add(str.length()-lastSize);
+	    	lastSize=str.length();
+	    	}
+	    int rowNum=0;
+	    for(int i = str.length()-2; i>=0; i--)
+	    	{
+	    	if(str.substring(i,i+1).equals("X")&&str.length()%i==0)
+	    		{
+	    		rowNum=(str.length()-1)-i;
+	    		}
+	    	break;
+	    	}
+	    
+	    String[][] scytale = new String[rowNum][(str.length()/rowNum)];
+	    for(int x = 0; x<scytale[0].length; x++)
+	    	{
+	    	for(int y = 0; y<scytale.length; y++)
+	    		{
+	    		
+	    		}
 	    	}
 	   }
 
