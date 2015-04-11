0c0fe5d6d5cf3cb4989ab862df35ad778542963d
DonovanCo <donovanco@mullenmail.com>
2015-04-11 13:43:58 -0600

    Done.

--- src/Prob12.java
+++ src/Prob12.java
@@ -2,11 +2,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
-
 public class Prob12
 	{
-
 	public static void main(String[] args) throws IOException
 		{
 		Scanner file = new Scanner( new File( "Prob12.in.txt" ) );
@@ -14,20 +11,18 @@ public class Prob12
 	    int count = 0;
 	    ArrayList<Integer> spacing = new ArrayList<Integer>();
 	    int lastSize=0;
-	   
 	    while(file.hasNextLine())
 	    	{
 	    	str+=file.nextLine();
 	    	spacing.add(str.length()-lastSize);
 	    	lastSize=str.length();
 	    	}
-	    System.out.println(str);
+//	    System.out.println(str);
 	    int rowNum=0;
 	    String last=str.substring(0, str.length()-1);
 	    int xLoc=last.lastIndexOf("X");
 	    rowNum=(str.length()-1)-xLoc;
-	    System.out.println(rowNum);
-	    
+//	    System.out.println(rowNum);
 	    String[][] scytale = new String[rowNum][(str.length()/rowNum)];
 	    int ind = 0;
 	    for(int x = 0; x<scytale[0].length; x++)
@@ -46,7 +41,6 @@ public class Prob12
 	    		aftScy+=scytale[a][b];
 	    	}
 	    }
-	    
 	    int lastX=str.length()-1;
 	    for(int k = aftScy.length(); k>=0; k--)
 	    {
@@ -58,28 +52,44 @@ public class Prob12
 	    }
 	    String temp = aftScy.substring(0, lastX);
 	    aftScy=temp;
-	    System.out.println(aftScy);
+//	    System.out.println(aftScy);
 	    int shift = 6;
-	    System.out.println();
-	    //Email Mr. McGuire about char
+//	    System.out.println();
 	    char[] bob = aftScy.toCharArray();
-//	    System.out.println(bob[0]);
-//	    for(int c = 0; c<bob.length-3; c++)
-//	    {
-//	    	if((bob[c+1]-1)==bob[c])
-//	    	{
-//	    		shift=(bob[c]);
-//	    	}
-//	    }
-	    
+	    for(int c = 0; c<bob.length-3; c++)
+	    {
+	    	if((bob[c+1]-33)==bob[c]&&(bob[c+2]-29)==bob[c]&&(bob[c+3]-46)==bob[c])
+	    	{
+	    		shift=(bob[c]-68);
+	    	}
+	    }
+//	    System.out.println(shift);
 	    for(int fred = 0; fred<bob.length; fred++)
 	    {
+	    	if(97<=bob[fred]&&bob[fred]<=102)
+	    	{
+	    		bob[fred]+=26;
+	    	}
+	    	if(65<=bob[fred]&&bob[fred]<=70)
+	    	{
+	    		bob[fred]+=26;
+	    	}
 	    	bob[fred]=(char) (bob[fred]-6);
-	    	System.out.print(bob[fred]);
+//	    	System.out.print(bob[fred]);
 	    }
+//	    System.out.println();
+//	    System.out.println();
+	    int total = 0;
+    	int jeff = 0;
+    	for(int h = 0; h<bob.length; h++)
+    	{
+    		if(h==(total+spacing.get(jeff)))
+    		{
+    		System.out.println();
+    		total+=spacing.get(jeff);
+    		jeff++;
+    		}
+    		System.out.print(bob[h]);
+    	}
 	   }
-	
-
-		
-
 	}
